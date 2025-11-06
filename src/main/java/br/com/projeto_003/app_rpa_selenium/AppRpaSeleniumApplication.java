f246e91766c4fdcc31c1fbcc39ff540793b771a4
Lucas Vinicius <vinidev.eng@gmail.com>
2025-11-06 10:55:48 -0300

    Add files via upload

---
+++ src/main/java/br/com/projeto_003/app_rpa_selenium/AppRpaSeleniumApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto_003.app_rpa_selenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRpaSeleniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRpaSeleniumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto_003/app_rpa_selenium/AppRpaSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto_003.app_rpa_selenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRpaSeleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
